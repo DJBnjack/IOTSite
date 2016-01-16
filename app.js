54a03bf5bd51c82a497f42d1e9c7e10f5c64561a
Johannes Bertens <Johannes Bertens>
2016-01-16 15:30:53 +0100

    Added iothub

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+// Get IOTHub services
+var iothub = require('azure-iothub');
+var connectionString = 'HostName=temperature-test.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=0V7f5WAGeeGjQjsg05iCYXaryOfpxfTobqrxqdvBrzk=';
+var registry = iothub.Registry.fromConnectionString(connectionString);
+
 // Load the http module to create an http server.
 var http = require('http');
 var port = process.env.PORT || 1337;
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+    response.writeHead(200, {"Content-Type": "application/json"});
+    registry.list(function (err, deviceList) {
+        deviceList.forEach(function (device) {
+            response.write(device.deviceId + ': ' + JSON.stringify(device, null, 2));
+        });
+    });
 });
 
 // Listen on port 80, IP defaults to 127.0.0.1
